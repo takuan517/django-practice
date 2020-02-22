515f131489be8e2672fbfccf970b8bc02ee29a91
takuan517 <ishitaku-2000517@keio.jp>
2020-02-23 00:59:46 +0900

    update

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.conf.urls import url, include
+
+from . import views
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    url('', views.index, name='index'),
 ]
