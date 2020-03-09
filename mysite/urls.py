3b6fb17985b957c0e2e115bd7b473ae7922d83e2
takuan517 <ishitaku-2000517@keio.jp>
2020-03-10 00:35:17 +0900

    update

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url, include
+from django.urls import include, path
 
 urlpatterns = [
-    url('polls/', include('polls.urls')),
-    url('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
